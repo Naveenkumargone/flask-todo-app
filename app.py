8a667b1bd196e327f2e9f6e0628857964248a947
Naveenkumar Gone <naveenkumargone1234@gmail.com>
2024-10-11 10:27:02 +0530

    comments added for each functionality

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
-from bson import ObjectId, json_util  # Import ObjectId
+from bson import ObjectId, json_util
 
 app = Flask(__name__)
 
-# Connect to MongoDB
+# Connecting to MongoDB
 client: MongoClient = MongoClient("mongodb://localhost:27017/")
 db = client["flaskTodo_db"]
 todos = db["todos"]
@@ -15,13 +15,14 @@ def hello_world():
     return render_template("index.html")
 
 
+# Api to Get all Todos from Mongodb
 @app.route("/api/todos", methods=["GET"])
 def get_todos():
     try:
         all_todos = list(todos.find({}).sort("_id", -1))
         if not (all_todos):
             return {"message": "todos not found"}
-        # Use json_util to serialize MongoDB documents (including ObjectIds)
+        # json_util to serialize MongoDB documents
         return json_util.dumps(all_todos), 200
     except Exception as e:
         return jsonify({"error": e}), 500
@@ -38,7 +39,7 @@ def add_todo():
     return jsonify({"error": "Task is required"}), 400
 
 
-# API to Edit a todo
+# API to Edit a todo's Task
 @app.route("/api/editTodo/<id>", methods=["PUT"])
 def edit_todo(id):
     task = request.json["task"]
@@ -46,8 +47,9 @@ def edit_todo(id):
     todo = todos.find_one({"_id": ObjectId(id)})
     if todo:
         todos.update_one(
-            {"_id": ObjectId(id)},
-            {"$set": {"task": task, "updated_at": updated_at}})
+            {"_id": ObjectId(id)}, 
+            {"$set": {"task": task, "updated_at": updated_at}}
+        )
         return jsonify({"message": "Todo Updated successfully!"}), 201
     return jsonify({"error": "Task is required"}), 400
 
@@ -57,13 +59,13 @@ def edit_todo(id):
 def update_todo(id):
     TodoStatus = request.json["completed"]
     updatedAt = request.json["updatedAt"]
-    print('-----------', request.json)
+    print("-----------", request.json)
     todo = todos.find_one({"_id": ObjectId(id)})
     print(todo)
     if todo:
         todos.update_one(
-            {"_id": ObjectId(id)}, 
-            {"$set": {"completed": TodoStatus, "updated_at": updatedAt}}
+            {"_id": ObjectId(id)},
+            {"$set": {"completed": TodoStatus, "updated_at": updatedAt}},
         )
         return jsonify({"message": "Todo updated successfully!"})
     return jsonify({"error": "Todo not found"}), 404
@@ -78,5 +80,7 @@ def delete_todo(id):
     return jsonify({"error": "Todo not found"}), 404
 
 
+# It runs on port 5000
+# debug=True is used to automatically reload the server when you make changes to the code
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
